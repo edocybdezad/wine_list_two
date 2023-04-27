a5ea0efdcbf0a3f1e5e9e9dd52b905b063af2053
edocybdezad <luis.alttamirano@gmail.com>
2023-04-27 14:23:06 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,100 @@
+// import {wines } from "./wines.js"
+
+const properties = ['name','precio','cepas','dop']
+
+document.addEventListener('DOMContentLoaded', function() {
+    logJSONData().
+    then( wines => {
+
+        // console.log("then wines",wines);
+        const Whites = wines.filter( wine => wine.attributes.tipo == 'blanco')
+        // console.log("whites", Whites);
+        const Reds = wines.filter( wine => wine.attributes.tipo == 'tinto')
+        
+        const Cava = wines.filter( wine => wine.attributes.tipo === 'Espumoso') 
+        
+        const Crianza = wines.filter( wine => wine.attributes.edad === 'Crianza')
+        const Ribera = wines.filter( wine => wine.attributes.dop === 'D.O. Ribera del Duero')
+        const Valencia = wines.filter( wine => wine.attributes.dop === 'D.O. Valencia')
+        const Garnacha = wines.filter( wine => wine.attributes.cepas.includes('Garnacha'));
+        const Verdejo = wines.filter( wine => wine.attributes.cepas === 'Verdejo')
+        document.getElementById('Crianza').addEventListener('click', () => {fltrWines('Crianza',Crianza)});
+        document.getElementById('Ribera').addEventListener('click', () => {fltrWines('Ribera',Ribera)});
+        document.getElementById('Valencia').addEventListener('click',() => {fltrWines('Valencia',Valencia)});
+        document.getElementById('Garnacha').addEventListener('click', () => {fltrWines('Garnacha',Garnacha)});
+        document.getElementById('Verdejo').addEventListener('click', () => {fltrWines('Verdejo',Verdejo)});
+        document.getElementById('Espumosos').addEventListener('click', () => {fltrWines('Espumosos',Cava)});        
+        
+        mainf([Whites, Reds, Cava]); 
+
+    })
+});
+async function logJSONData() {
+    const response = await fetch('https://restaurantapi-vjlx.onrender.com/api/vinos');
+    const jsonData = await response.json();
+    // console.log("function",jsonData.data);
+    return jsonData.data;
+}
+  
+function card(data) {
+    // console.log(data.attributes)
+
+    const itemCard = document.createElement("div");
+    itemCard.className = 'item--card';
+    for (let i = 0; i < properties.length; i++) {
+        var item = document.createElement("div")
+        item.classList.add(properties[i])
+        
+        item.innerText = data.attributes[properties[i]]
+        itemCard.appendChild(item)         
+    }
+    return itemCard
+  }
+function mainf(categories) {
+    document.getElementById('buttons').style.display = 'none';
+    document.getElementById('mainPageBtn').style.display = 'none';    
+    const wine_sections = ['whites','reds','cava']
+    for (var i = 0; i < categories.length; i++){
+        main_list(categories[i], wine_sections[i]);
+    }
+}
+function hideShowElements(side_btn, visual){
+    if (document.getElementById('buttons').innerHTML != '') {
+        document.getElementById('buttons').innerHTML = '';
+    }
+    document.getElementById(side_btn).style.display = `${visual}`;
+    document.getElementById('whites').style.display = `${visual}`;
+    document.getElementById('reds').style.display = `${visual}`;
+    document.getElementById('cava').style.display = `${visual}`;
+}
+function backToHome(side_btn){
+    document.getElementById('mainPageBtn').removeEventListener('click', backToHome);
+    document.getElementById('buttons').innerHTML = '';
+    document.getElementById('buttons').style.display = 'none';
+    document.getElementById('btnTitle').innerHTML = '';
+    document.getElementById('btnTitle').style.display = 'none';
+    document.getElementById('mainPageBtn').style.display = 'none';
+    hideShowElements(side_btn, 'block');
+}
+
+function fltrWines(side_btn,Btn) {
+    hideShowElements(side_btn,'none');
+    document.getElementById('mainPageBtn').style.display = 'block';
+    document.getElementById('buttons').style.display = 'block';
+    document.getElementById('btnTitle').style.display = 'block';
+    document.getElementById('btnTitle').innerText = `Vinos ${side_btn}`;
+    Btn.forEach( (e) => {
+
+        var item = card(e);
+        document.getElementById('buttons').appendChild(item);       
+    })
+    document.getElementById('mainPageBtn').addEventListener('click', () => {backToHome(side_btn)});
+}
+
+function main_list(w, ws) {
+    w.forEach( (e) => {
+        var item = card(e);
+        document.getElementById(ws).appendChild(item);
+    })
+}
+
---
+++ readme.md
@@ -0,0 +1,37 @@
+# Wine List Two
+ Same as [Wine-List-One](https://github.com/edocybdezad/Wine-List-One.git) but using Strapi CMS - deployed on [Render](https://render.com/) - to store content. Also render provides the web services
+
+ This basic web page uses as UI base [Wine-List-UI](https://github.com/edocybdezad/Wine-List-UI.git).
+ 
+Note: As a free account in Render is used for this project. In 90 days render
+desactivates the database. PSQL db by the case.
+
+About strapi I found is not that easy as I heard (maybe for some devs it is). it is better to start the project with a hosted PSQL. See acknowledgements part
+
+## Installation
+- Use the code tab to download the zip file. 
+
+
+## Usage
+
+- Unzip the folder and run the file index.html using Live Server or similar. VScode has Live Server extension by default.
+
+## Technologies
+- HTML
+- CSS
+- JavaScript
+- Strapi *
+- Node.js *
+- Cloudinary *
+* Not required to run this repo
+
+## Screnshot
+
+![Mobile Small](./img2_s.JPG)
+![Mobile Large](./img1.JPG)
+![Larger Screens](./img3_t.JPG)
+
+
+## Acknowledgements
+- [Programming Night, Tutorial](https://www.youtube.com/watch?v=akvItcWW81g)
+- [Baptiste Studer](https://github.com/Baboo7), for his [Strapi Plugin Import Export Entries](https://github.com/Baboo7/strapi-plugin-import-export-entries.git)
